10b6d8d54ebff0e47161bb949e389a1aadf6514d
ComlanGiovanni <giovanni.comlan@yahoo.fr>
2024-09-27 22:11:00 +0200

    ultime fonction need to norm, make make a folder with a small makefile lol

--- train/train.c
+++ train/train.c
@@ -1,20 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   train.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gicomlan <gicomlan@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/27 13:24:09 by gicomlan          #+#    #+#             */
-/*   Updated: 2024/09/27 21:35:36 by gicomlan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>	 // write, fork, execve, dup2, close, chdir, STDERR_FILENO
 #include <sys/wait.h>   // waitpid
 #include <stdlib.h>	 // malloc, free, exit, EXIT_FAILURE, EXIT_SUCCESS
 #include <string.h>	 // strcmp
 #include <stdbool.h>	// bool, true, false
+#include <stdio.h>	  // printf
 
 #define PIPE "|"
 #define SEMICOLON ";"
@@ -28,38 +17,69 @@ typedef enum e_command_type
 	TYPE_CD
 }	t_command_type;
 
-typedef struct s_command
-{
-	char			**arguments;
-	t_command_type	type;
-}	t_command;
-
 typedef struct s_micro_shell
 {
-	pid_t		pid;
-	char		**argv;
-	char		**envp;
-	int			argc;
-	int			index;
-	int			exit_code;
-	int			pipe_fd[2];
-	int			prev_pipe_fd;
-	t_command	command;
-}	t_micro_shell;
+	pid_t		   pid;
+	char			**argv;
+	char			**envp;
+	int			 argc;
+	int			 index;
+	int			 exit_code;
+	int			 pipe_fd[2];
+	int			 prev_pipe_fd;
+	char			**arguments;
+	t_command_type  type;
+}   t_micro_shell;
 
-static	size_t	ft_strlen(char *string)
+// Fonction de debug pour afficher la structure t_micro_shell
+static void ft_print_micro_shell(t_micro_shell *shell)
 {
-	const char	*last_char_string_pointer;
+	int index = 0;
 
-	last_char_string_pointer = string;
-	if (!string)
-		return (0x0);
-	while (*last_char_string_pointer != '\0')
-		last_char_string_pointer++;
-	return (last_char_string_pointer - string);
+	printf("Micro Shell Structure:\n");
+	printf("PID: %d\n", shell->pid);
+	printf("Argc: %d\n", shell->argc);
+	printf("Index: %d\n", shell->index);
+	printf("Exit Code: %d\n", shell->exit_code);
+	printf("Pipe FD: [%d, %d]\n", shell->pipe_fd[0], shell->pipe_fd[1]);
+	printf("Prev Pipe FD: %d\n", shell->prev_pipe_fd);
+	printf("Type: ");
+	if (shell->type == TYPE_NONE)
+		printf("TYPE_NONE\n");
+	else if (shell->type == TYPE_PIPE)
+		printf("TYPE_PIPE\n");
+	else if (shell->type == TYPE_SEMICOLON)
+		printf("TYPE_SEMICOLON\n");
+	else if (shell->type == TYPE_CD)
+		printf("TYPE_CD\n");
+	else
+		printf("UNKNOWN\n");
+	printf("Arguments:\n");
+	if (shell->arguments)
+	{
+		while (shell->arguments[index])
+		{
+			printf("  arguments[%d]: %s\n", index, shell->arguments[index]);
+			index++;
+		}
+	}
+	else
+		printf("  No arguments\n");
 }
 
-static void	ft_putstr_fd(char *string, int fd)
+// Autres fonctions inchangées, sauf pour les modifications mentionnées
+static size_t   ft_strlen(char *string)
+{
+	size_t length = 0;
+
+	if (!string)
+		return (0);
+	while (string[length] != '\0')
+		length++;
+	return (length);
+}
+
+static void ft_putstr_fd(char *string, int fd)
 {
 	if (!string)
 		string = "(null)";
@@ -67,49 +87,44 @@ static void	ft_putstr_fd(char *string, int fd)
 		write(fd, string, ft_strlen(string));
 }
 
-static void	ft_print_error(char *message)
+static void ft_print_error(char *message)
 {
 	ft_putstr_fd("error: ", STDERR_FILENO);
 	ft_putstr_fd(message, STDERR_FILENO);
 }
 
-static void	ft_fatal_error(void)
+static void ft_fatal_error(void)
 {
 	ft_print_error("fatal\n");
 	exit(EXIT_FAILURE);
 }
 
-static int ft_execute_cd(char **command)
+static int ft_execute_cd(char **arguments)
 {
-	char *path_to_change;
-
-	path_to_change = NULL;
-	if (!command[1] || command[2])
+	if (!arguments[1] || arguments[2])
 	{
 		ft_print_error("cd: bad arguments\n");
 		return (EXIT_FAILURE);
 	}
-	path_to_change = command[1];
-	if (chdir(path_to_change) == -1)
+	if (chdir(arguments[1]) == -1)
 	{
 		ft_print_error("cd: cannot change directory to ");
-		ft_putstr_fd(path_to_change, STDERR_FILENO);
+		ft_putstr_fd(arguments[1], STDERR_FILENO);
 		ft_putstr_fd("\n", STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
 
-static char	**ft_get_command_arguments(t_micro_shell *shell, \
-	int start, int end)
+static char **ft_get_command_arguments(t_micro_shell *shell, int start, int end)
 {
 	char	**cmd_arguments;
-	int		index;
+	int	 index;
 
 	cmd_arguments = (char **)malloc(sizeof(char *) * (end - start + 1));
 	if (!cmd_arguments)
 		ft_fatal_error();
-	index = 0x0;
+	index = 0;
 	while (start < end)
 	{
 		cmd_arguments[index] = shell->argv[start];
@@ -120,57 +135,68 @@ static char	**ft_get_command_arguments(t_micro_shell *shell, \
 	return (cmd_arguments);
 }
 
-static void	ft_close_fd(int fd)
+static void ft_close_fd(int fd)
 {
 	if (fd != -1 && close(fd) == -1)
 		ft_fatal_error();
 }
 
-static void	ft_setup_pipe(t_micro_shell *shell)
+static void ft_setup_pipe(t_micro_shell *shell)
 {
-	if (shell->command.type == TYPE_PIPE)
+	if (shell->type == TYPE_PIPE)
 		if (pipe(shell->pipe_fd) == -1)
+		{
+			perror("pipe"); // Debug
 			ft_fatal_error();
+		}
 }
 
-static void	ft_cannot_execute_commands(t_micro_shell *shell)
+static void ft_cannot_execute_commands(t_micro_shell *shell)
 {
 	ft_print_error("cannot execute ");
-	ft_putstr_fd(shell->command.arguments[0], STDERR_FILENO);
+	ft_putstr_fd(shell->arguments[0], STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
 
-static void	ft_execute_child_process(t_micro_shell *shell)
+static void ft_execute_child_process(t_micro_shell *shell)
 {
-	//setup_child_pipe
-		//if (input_fd != 0)??
+	printf("In child process (PID: %d)\n", getpid()); // Debug
+	ft_print_micro_shell(shell); // Debug
 	if (shell->prev_pipe_fd != -1)
 		if (dup2(shell->prev_pipe_fd, STDIN_FILENO) == -1)
 			ft_fatal_error();
-	if (shell->command.type == TYPE_PIPE)
+	if (shell->type == TYPE_PIPE)
 		if (dup2(shell->pipe_fd[STDOUT_FILENO], STDOUT_FILENO) == -1)
 			ft_fatal_error();
 	ft_close_fd(shell->prev_pipe_fd);
-	if (shell->command.type == TYPE_PIPE)//merci with other if
+	if (shell->type == TYPE_PIPE)
 	{
 		ft_close_fd(shell->pipe_fd[STDIN_FILENO]);
 		ft_close_fd(shell->pipe_fd[STDOUT_FILENO]);
 	}
-	if (execve(shell->command.arguments[0], \
-		shell->command.arguments, shell->envp) == -1)
+	if (execve(shell->arguments[0], shell->arguments, shell->envp) == -1)
 		ft_cannot_execute_commands(shell);
 }
 
-static void	ft_execute_parent_process(t_micro_shell *shell)
+static void ft_execute_parent_process(t_micro_shell *shell)
 {
-	//int status;
-	//waitpid(shell->pid, &status, 0); display with hook on wait in struct ?
-	// if (waitpid(pid, &status, 0) == -1)
-	// ft_fatal_error
-	waitpid(shell->pid, NULL, 0);
+	int status;
+
+	if (waitpid(shell->pid, &status, 0) == -1)
+		ft_fatal_error();
+	if (WIFEXITED(status))
+	{
+		printf("Child exited with status %d\n", WEXITSTATUS(status)); // Debug
+		shell->exit_code = WEXITSTATUS(status);
+	}
+	else
+	{
+		printf("Child terminated abnormally\n"); // Debug
+		shell->exit_code = EXIT_FAILURE;
+	}
 	ft_close_fd(shell->prev_pipe_fd);
-	if (shell->command.type == TYPE_PIPE)
+	if (shell->type == TYPE_PIPE)
 	{
 		ft_close_fd(shell->pipe_fd[STDOUT_FILENO]);
 		shell->prev_pipe_fd = shell->pipe_fd[STDIN_FILENO];
@@ -179,7 +205,7 @@ static void	ft_execute_parent_process(t_micro_shell *shell)
 		shell->prev_pipe_fd = -1;
 }
 
-static int	ft_execute_external_command(t_micro_shell *shell)
+static int ft_execute_external_command(t_micro_shell *shell)
 {
 	ft_setup_pipe(shell);
 	shell->pid = fork();
@@ -192,120 +218,117 @@ static int	ft_execute_external_command(t_micro_shell *shell)
 	return (EXIT_SUCCESS);
 }
 
-static void	ft_skip_semicolons(t_micro_shell *shell)
+static void ft_skip_semicolons(t_micro_shell *shell)
 {
 	while (shell->index < shell->argc && \
 		strcmp(shell->argv[shell->index], SEMICOLON) == 0)
-	{
-		shell->argv[shell->index] = NULL; //here
 		shell->index++;
-	}
 }
 
-static int	ft_get_command_end(t_micro_shell *shell)
+static int ft_get_command_end(t_micro_shell *shell)
 {
-	int	index;
+	int index;
 
 	index = shell->index;
-	while (index < shell->argc && \
-		strcmp(shell->argv[index], PIPE) != 0 && \
-			strcmp(shell->argv[index], SEMICOLON) != 0)
-				index++;
+	while (index < shell->argc &&
+		   strcmp(shell->argv[index], PIPE) != 0 &&
+		   strcmp(shell->argv[index], SEMICOLON) != 0)
+		index++;
 	return (index);
 }
 
-static bool	ft_is_pipe(t_micro_shell *shell)
+static bool ft_is_pipe(t_micro_shell *shell)
 {
-	if (shell->index < shell->argc && \
-		strcmp(shell->argv[shell->index], PIPE) == 0)
+	if (shell->index < shell->argc && strcmp(shell->argv[shell->index], PIPE) == 0)
 		return (true);
 	return (false);
 }
 
-static bool	ft_is_semicolon(t_micro_shell *shell)
+static bool ft_is_semicolon(t_micro_shell *shell)
 {
-	if (shell->index < shell->argc && \
-		strcmp(shell->argv[shell->index], SEMICOLON) == 0)
+	if (shell->index < shell->argc && strcmp(shell->argv[shell->index], SEMICOLON) == 0)
 		return (true);
 	return (false);
 }
 
-static void	ft_check_if_cd(t_micro_shell *shell)
+static void ft_check_if_cd(t_micro_shell *shell)
 {
-	if (shell->command.arguments[0] && \
-		strcmp(shell->command.arguments[0], CD) == 0)
-		shell->command.type = TYPE_CD;
+	if (shell->arguments[0] && strcmp(shell->arguments[0], CD) == 0)
+		shell->type = TYPE_CD;
 }
 
-static void	ft_parse_arguments(t_micro_shell *shell)
+static void ft_parse_arguments(t_micro_shell *shell)
 {
-	int	start;
-	int	end;
+	int start;
+	int end;
 
 	ft_skip_semicolons(shell);
 	if (shell->index >= shell->argc)
-		return ;
+		return;
 	start = shell->index;
 	end = ft_get_command_end(shell);
-	shell->command.arguments = ft_get_command_arguments(shell, start, end);
+	shell->arguments = ft_get_command_arguments(shell, start, end);
 	shell->index = end;
 	if (ft_is_pipe(shell))
 	{
-		shell->command.type = TYPE_PIPE;
+		shell->type = TYPE_PIPE;
 		shell->index++;
 	}
 	else if (ft_is_semicolon(shell))
 	{
-		shell->command.type = TYPE_SEMICOLON;
+		shell->type = TYPE_SEMICOLON;
 		shell->index++;
 	}
 	else
-		shell->command.type = TYPE_NONE;
-	shell->argv[shell->index] == NULL; //here
+		shell->type = TYPE_NONE;
 	ft_check_if_cd(shell);
+	ft_print_micro_shell(shell); // Debug
 }
 
-static int	ft_execute_command(t_micro_shell *shell)
+static int ft_execute_command(t_micro_shell *shell)
 {
-	int	exit_code;
+	int exit_code;
 
 	exit_code = EXIT_FAILURE;
-	if (shell->command.type == TYPE_CD)
-		exit_code = ft_execute_cd(shell->command.arguments);
+	printf("Executing command...\n"); // Debug
+	ft_print_micro_shell(shell);	 // Debug
+	if (shell->type == TYPE_CD)
+		exit_code = ft_execute_cd(shell->arguments);
 	else
 		exit_code = ft_execute_external_command(shell);
 	return (exit_code);
 }
 
-static void	ft_initialize_micro_shell(t_micro_shell *shell, \
-	int argc, char **argv, char **envp)
+static void ft_initialize_micro_shell(t_micro_shell *shell, int argc, char **argv, char **envp)
 {
 	shell->argv = argv;
 	shell->envp = envp;
 	shell->argc = argc;
-	shell->index = 0x1;
+	shell->index = 1;
 	shell->prev_pipe_fd = -1;
 	shell->exit_code = EXIT_SUCCESS;
-	shell->command.type = TYPE_NONE;
-	shell->command.arguments = NULL;
+	shell->arguments = NULL;
+	shell->type = TYPE_NONE;
 }
 
-int	main(int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	t_micro_shell	shell;
+	t_micro_shell shell;
 
+	printf("Starting microshell...\n"); // Debug
 	ft_initialize_micro_shell(&shell, argc, argv, envp);
 	while (shell.index < shell.argc)
 	{
-		shell.command.arguments = NULL;
-		shell.command.type = TYPE_NONE;
+		shell.arguments = NULL;
+		shell.type = TYPE_NONE;
 		ft_parse_arguments(&shell);
-		if (shell.command.arguments)
+		if (shell.arguments)
 		{
 			shell.exit_code = ft_execute_command(&shell);
-			free(shell.command.arguments);
-			shell.command.arguments = NULL;
+			free(shell.arguments);
+			shell.arguments = NULL;
 		}
 	}
+	printf("Microshell exited with code %d\n", shell.exit_code); // Debug
 	return (shell.exit_code);
 }
